3f643115bf5a6549b436d26905eeddf81851c0a7
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-09-14 23:13:18 -0600

    Update thought model with reaction schema and create reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
 const thoughtSchema = new Schema(
   {
@@ -17,6 +18,7 @@ const thoughtSchema = new Schema(
       type: String,
       required: true,
     },
+    reactions: [reactionSchema],
   },
   {
     toJSON: {

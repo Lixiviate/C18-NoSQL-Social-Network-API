8580f2b2e6d4d6b43d7e466e689dbaf22cc5c75f
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-09-14 23:41:22 -0600

    Add user/thought controllers and update routes for both

---
+++ controllers/thoughtController.js
@@ -0,0 +1,13 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
